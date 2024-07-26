d9542b6d7f63a59b937427554522ee147649c005
Betty Jimenez <bettyjimenez3010@gmail.com>
2024-07-26 17:39:52 -0400

    :ambulance: it is working in progress

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,17 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var react_1 = __importDefault(require("react"));
-var client_1 = __importDefault(require("react-dom/client"));
-var App_1 = __importDefault(require("./App"));
-var reportWebVitals_1 = __importDefault(require("./reportWebVitals"));
-var root = client_1.default.createRoot(document.getElementById('root'));
-root.render((0, jsx_runtime_1.jsx)(react_1.default.StrictMode, { children: (0, jsx_runtime_1.jsx)(App_1.default, {}) }));
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-(0, reportWebVitals_1.default)();
+__exportStar(require("./modules/dropdown/DropdownSelector"), exports);
---
+++ src/index.d.ts
@@ -0,0 +1,20 @@
+declare module 'musa-design-system' {
+  import * as React from 'react';
+
+  export interface OptionsType {
+    value: string | null;
+    label: string;
+  }
+
+  interface DropdownSelectorProps {
+    selectedOption: OptionsType | null;
+    setSelectedOption: (option: OptionsType | null) => void;
+    options: OptionsType[];
+    labelText: string;
+    hintText: string;
+  }
+
+  const DropdownSelector: React.FC<DropdownSelectorProps>;
+
+  export default DropdownSelector;
+}
